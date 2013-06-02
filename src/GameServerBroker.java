5be87f894fcc7be8d10a51d4529e3de407eb2119
Marth Aurion <marthaurion@gmail.com>
2013-06-02 09:26:34 -0500

    blah

--- src/GameServerBroker.java
+++ src/GameServerBroker.java
@@ -21,9 +21,12 @@ public class GameServerBroker {
 		while(true) {
 			
 			Socket p1 = socket.accept();
+			System.out.println("Player 1 connected");
 			Socket p2 = socket.accept();
+			System.out.println("Player 2 connected");
 			GameServerThread g1 = new GameServerThread(p1, PieceColor.White);
 			GameServerThread g2 = new GameServerThread(p2, PieceColor.Black);
+			System.out.println("Game Starting");
 			new GameServerGame(g1, g2);
 		}
 		
