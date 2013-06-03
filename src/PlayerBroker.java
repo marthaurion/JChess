ef374d7fcce8c2af7dde49f4aab7c129985bb057
Marth Aurion <marthaurion@gmail.com>
2013-06-02 22:03:52 -0500

    ip change

--- src/PlayerBroker.java
+++ src/PlayerBroker.java
@@ -7,26 +7,19 @@ import java.net.Socket;
 
 public class PlayerBroker {
 	
-	private GameServerBroker broker;
 	private Socket socket;
 	private PrintWriter output;
 	private BufferedReader input;
 	
 	public PlayerBroker() {
-		broker = new GameServerBroker("68.51.78.38");
 		socket = null;
 		output = null;
 		input = null;
 	}
 	
-	public void attachServer(GameServerBroker b) {
-		broker = b;
-	}
-	
 	public PieceColor startGame() throws IOException {
-		if(broker == null) return PieceColor.None;
 		
-		socket = new Socket(broker.getIP(), 8484);
+		socket = new Socket("68.51.78.38", 8484);
 		input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		output = new PrintWriter(socket.getOutputStream(), true);
 		System.out.println("Reading Color");
