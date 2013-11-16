5a849060022a50e5985060e30a4237dbf5f66c0d
marthaurion <marthaurion@gmail.com>
2013-11-16 16:44:18 -0600

    use images to display pieces

--- src/ChessButton.java
+++ src/ChessButton.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 
@@ -10,15 +13,37 @@ public class ChessButton extends JButton {
 		myX = x;
 		myY = y;
 		setBorderPainted(true);
+		if((x+y)%2 == 1) setBackground(Color.BLACK);
+		else setBackground(Color.WHITE);
 	}
 	
 	public void setPiece(Piece p) {
-		String st = "";
-		if(p.getName().equals("None")) st = " ";
-		else if(p.getName().equals("Pawn")) st += p.getColor().toString().substring(0,1)+"P";
-		else st += p.getColor().toString().substring(0,1)+p.getID();
+		//if setting to an empty square, just remove icon and return
+		if(p.getColor() == PieceColor.None) {
+			setIcon(null);
+			return;
+		}
 		
-		setText(st);
+		
+		//otherwise create the path to the image and display it
+		String st = "images/";
+		
+		if(p.getColor() == PieceColor.White) st += "White ";
+		else if(p.getColor() == PieceColor.Black) st += "Black ";
+		
+		if(p.getName().equals("Pawn")) st += "P.png";
+		else if(p.getName().equals("King")) st += "K.png";
+		else if(p.getName().equals("Knight")) st += "N.png";
+		else if(p.getName().equals("Bishop")) st += "B.png";
+		else if(p.getName().equals("Queen")) st += "Q.png";
+		else if(p.getName().equals("Rook")) st += "R.png";
+		else {
+			System.out.println("Something went wrong.");
+			System.exit(0);
+		}
+		
+		setIcon(new ImageIcon(st));
+			
 	}
 	
 	public void activate() {
